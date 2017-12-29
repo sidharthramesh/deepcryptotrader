0a20748430a05625a364a9528b077423301abe6a
Sidharth Ramesh <tornadoalert@gmail.com>
2017-12-29 20:02:02 +0530

    added search test

--- test_twitter.py
+++ test_twitter.py
@@ -3,3 +3,10 @@ from twitter_bot import TwitterBot
 def test_TwitterBot():
     bot = TwitterBot()
     assert bot != None, "Check twitter env variables"
+
+def test_search_tweets():
+    bot = TwitterBot()
+    searches=bot.api.GetSearch('BTC',count=1)
+    print("[o] Searching twitter for BTC")
+    print(searches[0].text)
+    
